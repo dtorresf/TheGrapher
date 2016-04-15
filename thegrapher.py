9b46a42ccfa286b33fb60c9e3cf049b0106af864
Daniela Torres <daniela.torres.f@gmail.com>
2016-04-15 12:49:17 -0500

    First commit
    First commit for master

--- thegrapher.py
+++ thegrapher.py
@@ -3,5 +3,9 @@
 from Server import Server
 import methodServer
 
+### Prueba de Calculo de promedios 
+print("Promedio de CPU: ",s.meancpu())
+
 s = methodServer.importdatatoserver('/Users/daniela/DevOps/TheGrapher/data/el01cn09_osmonitor.csv')
 s.graphserver()
+
